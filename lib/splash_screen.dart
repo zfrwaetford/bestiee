d60bcc1d9e817ed4459ea192b6153402bd3bb0dd
Sanggana Praba <sangganapraba@gmail.com>
2021-10-02 11:33:03 +0700

    feat [SP] splash screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =d> Home()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Image.asset('images/burger.png', width: 300),
+        ),
+      ),
+    );
+  }
+}
